b9fdec6406f5bbbfb35f6ed0f86dadb559ec8b11
Gabriella Oliveira <gabriellapereira101@gmail.com>
2022-09-20 23:51:30 -0300

    New Autthorization e Authentication.

--- Sistemadeagendamentodeconsulta/Startup.cs
+++ Sistemadeagendamentodeconsulta/Startup.cs
@@ -64,6 +64,7 @@ namespace Sistemadeagendamentodeconsulta
             .AllowAnyHeader());
 
             app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
